c7329b2ab31f80b0570b645de5ee9522214b923a
Austin Willoughby <arwigm@rit.edu>
2025-09-05 14:44:27 -0400

    Added basic server

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+
+const onRequest = (request, response) => {
+  if (request.url === '/') {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(index);
+    response.end();
+  } else {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('No page here!');
+    response.end();
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
