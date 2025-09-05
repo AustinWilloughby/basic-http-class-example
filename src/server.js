8fec36ccb7fbd2a77a379a577f2c488d90b21442
Austin Willoughby <arwigm@rit.edu>
2025-09-05 14:47:41 -0400

    forced error

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
 const onRequest = (request, response) => {
+  const test = 1;
+
   if (request.url === '/') {
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(index);
